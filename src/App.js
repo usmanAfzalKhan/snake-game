2c8ae3dba9c0f050b2b8678de17ef409e69afdba
usmanAfzalKhan <usmanafzal.khan2001@gmail.com>
2025-05-05 16:05:11 -0400

    Initial commit - React app setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Snake Game 🐍</h1>
+      <p>Game page coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
